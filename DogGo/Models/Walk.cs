7d7229272ef19c5b025ac5bb35959b47d7449092
DougSilvera <shortxjdoug@gmail.com>
2022-03-30 16:23:04 -0400

    walker profile

--- DogGo/Models/Walk.cs
+++ DogGo/Models/Walk.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 namespace DogGo.Models
 {
     public class Walk
     {
         public int Id { get; set; }
+        [DisplayFormat(DataFormatString = "{0:MMM dd, yyyy}")]
         public DateTime Date { get; set; }
         public int Duration { get; set; }
         public int WalkerId { get; set; }
@@ -11,5 +13,15 @@ namespace DogGo.Models
         public Walker Walker { get; set; }
         public Dog Dog { get; set; }
         public Owner Owner { get; set; }
+        public string WalkDuration
+        {
+            get
+            {
+                var totalMinutes = Duration / 60;
+                var totalHours = totalMinutes / 60;
+                var minutes = totalMinutes % 60;
+                return $"{totalHours} hrs {minutes} min";
+            }
+        }
     }
 }
