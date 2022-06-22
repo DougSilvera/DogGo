e393f8e7e82933ff1def2557d5fc2ad3d4156471
DougSilvera <shortxjdoug@gmail.com>
2022-06-22 16:30:20 -0400

    final push

--- DogGo/Interfaces/IWalksRepository.cs
+++ DogGo/Interfaces/IWalksRepository.cs
@@ -6,5 +6,8 @@ namespace DogGo.Interfaces
     public interface IWalksRepository
     {
         List<Walk> GetWalksByWalkerId(int walkerId);
+        void CreateWalk(Walk walk);
+        List<Walk> GetAllWalks();
+        void DeleteWalk(int id);
     }
 }
